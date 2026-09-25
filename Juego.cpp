5dd748ccc08a135ff1ade69f5eb9d4c44faf2044
NILSON <cardenas312nilson@gmail.com>
2026-09-24 19:21:50 -0500

    cambio

--- Juego.cpp
+++ Juego.cpp
@@ -1,4 +1,9 @@
-
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
 // ======================================
 // CLASE: Juego
 // ======================================
